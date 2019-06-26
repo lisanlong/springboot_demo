faa66ed920463e323d21bb749e2b7dd9390dc98d
lisanlong <1119753268@qq.com>
2019-06-26 16:44:51 +0800

    Initial commit

---
+++ src/main/java/com/afn/springboot_demo/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.afn.springboot_demo.repository;
+
+import com.afn.springboot_demo.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u")
+    Page<User> findList(Pageable pageable);
+    User findById(long id);
+    User findByUserName(String userName);
+    void deleteById(Long id);
+    User findByUserNameAndPassWord(String userName,String passWord);
+
+}
---
+++ src/main/java/com/afn/springboot_demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.afn.springboot_demo.service;
+
+
+import com.afn.springboot_demo.model.User;
+import org.springframework.data.domain.Page;
+
+public interface UserService {
+
+
+
+
+
+
+}
